0289f6d6b5a3d8148e1e0f58f71ce05c68a1cd13
Tomas Mendoza <tm1453766@gmail.com>
2025-02-22 15:48:16 -0300

    first commit

---
+++ Backend/src/main/java/com/alquila_tu_espacio/AlquilaTuEspacioApplication.java
@@ -0,0 +1,13 @@
+package com.alquila_tu_espacio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilaTuEspacioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilaTuEspacioApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/alquila_tu_espacio/AlquilaTuEspacioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alquila_tu_espacio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilaTuEspacioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
